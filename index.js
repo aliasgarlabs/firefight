160daff5820e9b6505d03e0343ca324392b88951
Aliasgar Murtaza <aliasgarlabs@gmail.com>
2023-12-04 16:30:34 +0400

    Init commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/user/:id', (req, res) => {
+  var request_id = req.params.id;
+
+  var database = [
+    {
+      id: 'tb',
+      name: "Taha Murtaza"
+    },
+    {
+      id: 'ab',
+      name: "Aliasgar Murtaza"
+    }
+  ]
+
+  var result = database.find((i) => i.id === request_id).name;
+
+  res.send(`Hello ${result}`)
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
