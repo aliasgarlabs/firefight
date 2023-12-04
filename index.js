f100b3d3cf00ea04b59769c42ffbcc87b97c1571
Aliasgar Murtaza <aliasgarlabs@gmail.com>
2023-12-04 18:11:58 +0400

    style

--- index.js
+++ index.js
@@ -12,13 +12,44 @@ app.get('/user/:id', (req, res) => {
     },
     {
       id: 'ab',
-      name: "Aliasgar Murtaza"
+      name: "Aliasgar Murtaza Bombaywala"
     }
   ];
 
   const result = database.find(item => item.id === requestId).name;
 
-  res.send(`Hello ${result}`);
+  // HTML and CSS for a basic design
+  const htmlContent = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Express App</title>
+      <style>
+        body {
+          font-family: Arial, sans-serif;
+          background-color: #f0f0f0;
+          text-align: center;
+          margin: 20px;
+        }
+        .container {
+          background-color: #fff;
+          padding: 20px;
+          border-radius: 8px;
+          box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+      </style>
+    </head>
+    <body>
+      <div class="container">
+        <h1>Hello ${result}</h1>
+      </div>
+    </body>
+    </html>
+  `;
+
+  res.send(htmlContent);
 });
 
 app.listen(port, () => {
